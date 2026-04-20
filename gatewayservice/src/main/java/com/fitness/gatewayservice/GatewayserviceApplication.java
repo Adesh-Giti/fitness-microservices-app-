b072546369974d5afd27beb7908ed4bbbd65295e
Adesh Chaudhary <chaudharyadesh250@gmail.com>
2026-04-20 22:47:21 +0530

    Implemented the Api Gateway in the application and able to access through api gateway.

---
+++ gatewayservice/src/main/java/com/fitness/gatewayservice/GatewayserviceApplication.java
@@ -0,0 +1,28 @@
+package com.fitness.gatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class GatewayserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayserviceApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route("user-service", r -> r.path("/api/users/**")
+						.uri("lb://USER-SERVICE"))
+                .route("activity-service",r->r.path("/api/activities/**")
+                        .uri("lb://ACTIVITY-SERVICE"))
+                .route("ai-service",r->r.path("/api/recommendations/**")
+                        .uri("lb://AI-SERVICE"))
+				.build();
+
+	}
+}
